59ff4dfc0a35305bb04e70fba93b5a3dcf2fd355
Fatiya Humaira Yunaz <fatiya33@mhs.unsyiah.ac.id>
2024-09-05 21:02:41 +0700

    show img

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import os
 
 app = Flask(__name__, static_folder='src', static_url_path='')
 app.secret_key = 'your_secret_key'  # Kunci rahasia untuk flash messages
-app.config['UPLOAD_FOLDER'] = 'static/uploads/'  # Simpan gambar di folder static/uploads
+app.config['UPLOAD_FOLDER'] = 'src/static/uploads/'  # Simpan gambar di folder static/uploads
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}  # Ekstensi file yang diperbolehkan
 
 # Fungsi untuk memeriksa apakah file memiliki ekstensi yang diizinkan
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/histogram.html', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # Cek apakah request berisi file
@@ -30,18 +30,18 @@ def upload_file():
             filename = secure_filename(file.filename)  # Amankan nama file
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)  # Simpan file di folder yang ditentukan
-            
+
             # Redirect ke halaman upload dengan gambar yang sudah diunggah
             return redirect(url_for('upload_file', filename=filename))
 
     # Tampilkan halaman upload
     filename = request.args.get('filename')
-    return render_template('homepage.html', filename=filename)
+    return render_template('histogram.html', filename=filename)
 
 # Route untuk halaman histogram.html
-@app.route('/histogram.html')
-def histogram():
-    return render_template('histogram.html')
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
